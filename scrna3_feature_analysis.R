1f03fbce88b0da81bc68225e2f32132c7f098978
Rajarshi Ray <rayrajarshi9@gmail.com>
2025-02-19 22:56:03 +0530

    Feature analysis and condition assesement

--- scrna3_feature_analysis.R
+++ scrna3_feature_analysis.R
@@ -1,19 +1,86 @@
- # Import libraries
+# Define libraries
 
 library(Seurat)
+library(SeuratData)
 library(tidyverse)
 
-# Data loading
+# Define dataset
 
-sc.data <- readRDS('seurat_integrated.rds')
-View(sc.data)
-View(sc.data@meta.data)
-# Visualise Data clusters
+ifnb <- readRDS('ifnb_harmony.rds')
+View(ifnb@meta.data)
 
-clusters <- DimPlot(sc.data,reduction = 'umap',group.by = 'seurat_clusters',label = T)
-clusters
+# visualize data
+clusters <- DimPlot(ifnb, reduction = 'umap', group.by = 'seurat_clusters', label = TRUE)
+condition <- DimPlot(ifnb, reduction = 'umap', group.by = 'stim')
 
-background <- DimPlot(sc.data,reduction = 'umap',group.by = 'Type')
-background
+condition|clusters
 
-clusters | background
+# Find all the DEG biomarkers in the dataset across each cell types
+
+FindAllMarkers(ifnb,
+               logfc.threshold = 0.25,
+               min.pct = 0.1,
+               only.pos = TRUE,
+               test.use = 'DESeq2',
+               slot = 'counts')
+
+# Find conserved markers across condition types across cell type
+
+marker.feature.cluster.3 <- FindConservedMarkers(ifnb,
+                                                 ident.1 = 3,
+                                                 grouping.var = 'stim')
+
+View(marker.feature.cluster.3)
+
+# Visualise the top marker plot
+
+FeaturePlot(ifnb,features = c('FCGR3A'),min.cutoff = 'q10')
+
+# Now we move on to annotate the cell types based on meta data
+
+View(ifnb@meta.data)
+
+## To see the name of cell barcodes
+
+Idents(ifnb)
+
+## Set the names to seurat annotations
+
+Idents(ifnb) <- ifnb@meta.data$seurat_annotations
+View(Idents(ifnb))
+
+
+DimPlot(ifnb,reduction = 'umap', label = T)
+
+# Find marker genes between conditions
+
+ifnb$celltype.cond <- paste0(ifnb$seurat_annotations,'_',ifnb$stim)
+
+ifnb$celltype.cond
+
+## Now incorporate the newly condition based annotation in the meta data
+
+Idents(ifnb) <- ifnb$celltype.cond
+
+View(ifnb@meta.data)
+
+DimPlot(ifnb, reduction = 'umap', label = TRUE)
+
+cd16.response <- FindMarkers(ifnb, ident.1 = 'CD16 Mono_STIM', ident.2 = 'CD16 Mono_CTRL')
+
+head(cd16.response)
+
+# plotting conserved features vs DE features between conditions
+head(marker.feature.cluster.3)
+
+
+FeaturePlot(ifnb, features = c('FCGR3A', 'AIF1', 'IFIT1'), split.by = 'stim', min.cutoff = 'q10')
+
+
+
+
+
+
+
+
+
